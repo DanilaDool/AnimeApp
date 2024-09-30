c1694507ddf1a580676943fce08d1f6627daa95e
DanilaDool <fruit.dan@mail.ru>
2024-10-01 00:57:51 +0300

    NotWorkSlider

--- app/models/anime.rb
+++ app/models/anime.rb
@@ -3,11 +3,12 @@ class Anime < ApplicationRecord
   include PgSearch::Model
 
   pg_search_scope :search_by_title_and_content,
-  against: [:title, :shikimori_id],
+  against: [:title, :shikimori_id, :date],
   using: {
      tsearch: { prefix: true },
      trigram: { threshold: 0.2 }
   }
+
 
   def in_current_season?
     current_month = Date.today.month
